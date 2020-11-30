0dbd880296c4257c2777e13b4d4dfb955a252cce
Amira Rashad <amira.rashad1996@hotmail.com>
2020-11-30 17:51:22 +0500

    update

--- myapp/routes/products.js
+++ myapp/routes/products.js
@@ -3,8 +3,19 @@ var router = express.Router();
 var Product = require("../DB/product");
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-    let products = await Product.find();
-    console.log(products);
-  res.render("products/list", {title: "Products in DB" });
+    let products =  Product.find();
+    nconsole.log(products);
+  res.render("products/list", {title: "Products in DB", products });
 });
+router.get('/add', async function(req, res, next) {
+   
+  res.render("products/add");
+});
+router.post('/add', async function(req, res, next) {
+    let  product= new Product(req.body);
+    await product.save();
+   
+    res.redirect("products");
+  });
+
 module.exports = router;
