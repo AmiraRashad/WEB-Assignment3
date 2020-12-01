eebfb23ccd8971a601d7764f8778999387a81fa4
Amira Rashad <amira.rashad1996@hotmail.com>
2020-12-01 20:12:33 +0500

    .

--- myapp/routes/products.js
+++ myapp/routes/products.js
@@ -3,19 +3,24 @@ var router = express.Router();
 var Product = require("../DB/product");
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-    let products =  Product.find();
-    nconsole.log(products);
-  res.render("products/list", {title: "Products in DB", products });
+    let products = await Product.find();
+    console.log(products);
+  res.render("/products/list", {title: "Products in DB", products });
 });
-router.get('/add', async function(req, res, next) {
+router.get("/add", async function(req, res, next) {
    
-  res.render("products/add");
+  res.render("/products/add");
 });
-router.post('/add', async function(req, res, next) {
+router.post("/add", async function(req, res, next) {
     let  product= new Product(req.body);
     await product.save();
-   
-    res.redirect("products");
+    res.redirect("/products");
+  });
+
+  router.get("/delete/:id", async function(req, res, next) {
+    let  product= new Product(req.body);
+    await product.save();
+    res.redirect("/products");
   });
 
 module.exports = router;
